61bf272742f473c805100af17e7e581d8f6d1952
LucasHausi <l.hausensteiner@gmail.com>
2025-09-08 14:04:15 +0200

    remove unused EntsoeRepository class and related file

--- src/probabilistic_load_forecast/repository/repository.py
+++
@@ -1,8 +0,0 @@
-class EntsoeRepository:
-    def __init__(self, fetcher, mapper):
-        self.fetcher = fetcher
-        self.mapper = mapper
-
-    def get_data(self, params):
-        raw_data = self.fetcher.fetch(params)
-        return self.mapper.map(raw_data)
